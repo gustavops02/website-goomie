4a4ce5e2578d5ded3845a24e539e246a79c40da9
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2024-05-20 20:07:36 -0300

    criação do componente navbar

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1> NAVBAR </h1>
+</template>
+
+
+<script>
+
+    export default {
+        name: 'NavbarComponent',
+        components: {
+
+        },
+
+        data() {
+
+        },
+
+        methods: {
+            
+        }
+    }
+
+</script>
+
+
+<style scoped>
+
+</style>
