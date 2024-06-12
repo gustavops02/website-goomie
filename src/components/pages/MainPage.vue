60ff59e6168a2f2e1350cfc403a9d5eaca7a8150
Guilherme P Silva <guilhermeps002@outlook.com>
2024-06-12 19:02:03 -0300

    Importação do footer

--- src/components/pages/MainPage.vue
+++ src/components/pages/MainPage.vue
@@ -6,43 +6,36 @@
             <ServicesComponent />
             <AboutComponent />
         </div>
+        <FooterComponent />
     </div>
-
 </template>
 
-
 <script>
+import NavbarComponent from "../NavbarComponent.vue";
+import HomeComponent from "@/components/HomeComponent.vue";
+import ServicesComponent from "@/components/ServicesComponent.vue";
+import AboutComponent from "@/components/AboutComponent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
 
-    import NavbarComponent from "../NavbarComponent.vue";
-    import HomeComponent from "@/components/HomeComponent.vue";
-    import ServicesComponent from "@/components/ServicesComponent.vue";
-    import AboutComponent from "@/components/AboutComponent.vue";
-
-    export default {
-        components:{
-            NavbarComponent,
-            HomeComponent,
-            ServicesComponent,
-            AboutComponent,
-        },
-
-        data() {
-            return {
-
-            }
-        }
-    }
+export default {
+    components:{
+        NavbarComponent,
+        HomeComponent,
+        ServicesComponent,
+        AboutComponent,
+        FooterComponent,
+    },
+}
 </script>
-
 
 <style>
 .container {
-    width: 100vw;
-    min-height: 100vh;    
-    .pages {
-        display: flex;
-        flex-direction: column;
-    }
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
 }
 
+.pages {
+    flex: 1;
+}
 </style>
