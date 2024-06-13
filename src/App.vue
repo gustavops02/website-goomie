d42459ea9089164c61b6b0740de844a530c624ec
Guilherme P Silva <guilhermeps002@outlook.com>
2024-06-12 21:06:26 -0300

    Degradê retirado

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,6 @@ export default {
 
 body {
   background: rgb(9,16,29);
-  background: linear-gradient(0deg, rgba(9,16,29,1) 3%, rgba(7,13,23,1) 31%, rgba(7,13,23,1) 50%, rgba(7,13,23,1) 71%, rgba(9,16,29,1) 97%); 
 }
 
 
